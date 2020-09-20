296a22e4462e62b1e78a7f72d419220e5fe8b3e4
SensiPhi <lailtoneletrohits@hotmail.com>
2020-09-20 10:19:28 -0300

    ThemeProvider, dark & light theme logic

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import dark from './styles/themes/dark';
+import light from './styles/themes/light';
+import GlobalStyle from './styles/global';
+import Header from './components/Header/Header';
+import Feed from './components/Feed/Feed';
 
 function App() {
-  return <div>opa</div>;
+  const [theme, setTheme] = React.useState(() => {
+    const storageValue = JSON.parse(localStorage.getItem('theme'));
+    if (storageValue && storageValue.title === 'dark') return dark;
+    else return light;
+  });
+
+  React.useEffect(() => {
+    localStorage.setItem('theme', JSON.stringify(theme));
+  }, [theme]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Header theme={theme} setTheme={setTheme} />
+      <Feed />
+    </ThemeProvider>
+  );
 }
 
 export default App;
