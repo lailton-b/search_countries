fddaffc1bf7c18ffe792b9128db16a96651d00ca
SensiPhi <lailtoneletrohits@hotmail.com>
2020-09-24 12:37:30 -0300

    Routes added

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import dark from './styles/themes/dark';
 import light from './styles/themes/light';
 import GlobalStyle from './styles/global';
 import Header from './components/Header/Header';
 import Feed from './components/Feed/Feed';
+import CountryPage from './components/CountryPage/CountryPage';
+import NotFound from './components/Helper/NotFound/NotFound';
 
 function App() {
   const [theme, setTheme] = React.useState(() => {
@@ -18,11 +21,21 @@ function App() {
   }, [theme]);
 
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Header theme={theme} setTheme={setTheme} />
-      <Feed />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Header theme={theme} setTheme={setTheme} />
+
+        <Routes>
+          <Route path="/search_countries" element={<Feed />} />
+          <Route
+            path="/search_countries/country/:name"
+            element={<CountryPage />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
