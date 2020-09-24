5cd2d258bf6e6fcbb964ee7338c3794d5e381b6b
SensiPhi <lailtoneletrohits@hotmail.com>
2020-09-24 12:34:02 -0300

    fetchAll edited & fetchByCode added

--- app/src/api.js
+++ app/src/api.js
@@ -1,8 +1,31 @@
 const URL_API = 'https://restcountries.eu/rest/v2';
 
-export function fetchAll() {
+export function fetchAll(fields) {
   return {
-    url: URL_API + '/all?fields=name;capital;population;region;flag',
+    url: URL_API + `/all?fields=${fields}`,
+    options: {
+      method: 'GET',
+    },
+  };
+}
+
+export function fetchCountry(name) {
+  return {
+    url: URL_API + `/name/${name}`,
+    options: {
+      method: 'GET',
+    },
+  };
+}
+
+export function fetchByCode(codes) {
+  let codeList = '';
+  codes.map((code, i) =>
+    i < codes.length - 1 ? (codeList += code + ';') : (codeList += code),
+  );
+
+  return {
+    url: URL_API + `/alpha?codes=${codeList}`,
     options: {
       method: 'GET',
     },
